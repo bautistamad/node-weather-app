d7fb450565178602cd327055dbcdb30e68f5748c
Bautista Madruga <bautistamadruga20@gmail.com>
2022-01-07 16:33:10 -0300

    search class added

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 
-const { leerInput } = require('./helpers/inquirer')
+const { leerInput, inquireMenu, pausa } = require('./helpers/inquirer');
+const Search = require('./models/search');
 
-const main = async() => {
+const main = async () => {
 
+    const search = new Search();
+    let opt;
 
-    const text = await leerInput('Hola');
+    do {
 
-    console.log( text )
+        opt = await inquireMenu()
+
+        switch (opt) {
+            case 1:
+                //Show message
+                const place =  await leerInput('Ciudad: ');
+                console.log(place);
+                //Select place
+
+                //Weather
+
+                //Show results
+                console.log('\nInformacion de la ciudad'.green)
+                console.log('Ciudad: ');
+                console.log('Lat: ');
+                console.log('Lng: ');
+                console.log('Temperatura: ');
+                console.log('Minima: ');
+                console.log('Maxima: ');
+
+                break;
+            default:
+                break;
+        }
+
+        await pausa();
+
+    } while (opt != 0);
+
 
 
 }
---
+++ models/search.js
@@ -0,0 +1,22 @@
+
+
+
+class Search {
+    
+    history = ['Tegucigalpa', 'Madriga', 'Buenos Aires']
+
+    constructor() {
+        //TODO: leer DB si existe
+    }
+
+     async city( place = '' ) {
+
+        //request http
+        console.log(place);
+
+        return []; // return  places
+     } 
+
+}
+
+module.exports = Search
